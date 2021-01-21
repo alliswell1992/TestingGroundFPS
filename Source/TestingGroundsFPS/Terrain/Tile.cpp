e6e7f10dba8b6ad3b329ba1ccfa1911014ecffa6
DESKTOP-BVJ8TRB\admin <liouliwoku1@163.com>
2021-01-21 08:35:43 +0800

    drawdebugcapsure  and custome collision channel

--- Source/TestingGroundsFPS/Terrain/Tile.cpp
+++ Source/TestingGroundsFPS/Terrain/Tile.cpp
@@ -40,8 +40,9 @@ void ATile::Tick(float DeltaTime)
 }
 bool ATile::CastSphere(FVector Location, float Radius) {
 	FHitResult HitResult;
-	bool HasHit = GetWorld()->SweepSingleByChannel(HitResult, Location, Location, FQuat::Identity, ECollisionChannel::ECC_Camera, FCollisionShape::MakeSphere(Radius));
+	bool HasHit = GetWorld()->SweepSingleByChannel(HitResult, Location, Location, FQuat::Identity, ECollisionChannel::ECC_GameTraceChannel2, FCollisionShape::MakeSphere(Radius));
 	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
-	DrawDebugSphere(GetWorld(), Location, Radius, 4, ResultColor, true, 100);
+	//DrawDebugSphere(GetWorld(), Location, Radius, 4, ResultColor, true, 100);
+	DrawDebugCapsule(GetWorld(), Location, 0, Radius, FQuat::Identity, ResultColor, true, 100);
 	return HasHit;
 }
